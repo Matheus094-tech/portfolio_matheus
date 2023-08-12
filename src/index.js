97866a9b2048b8c0c46bec273194642823b80681
Matheus094-tech <matheusbarbosa094@gmail.com>
2023-08-11 22:00:05 -0300

    ajuste diretorio

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-import "assets/css/nucleo-icons.css";
-import "assets/scss/blk-design-system-react.scss";
-import "assets/demo/demo.css";
+import "./assets/css/nucleo-icons.css";
+import "./assets/scss/blk-design-system-react.scss";
+import "./assets/demo/demo.css";
 
 import Index from "views/Index.js";
 
