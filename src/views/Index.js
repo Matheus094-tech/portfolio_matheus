f284b7c802cafc278b6fb4827ef376e869537c03
Matheus094-tech <matheusbarbosa094@gmail.com>
2023-08-12 14:12:45 -0300

    main

--- src/views/Index.js
+++ src/views/Index.js
@@ -10,7 +10,6 @@ import Sobre from "views/IndexSections/sobre_mim";
 import Curriculo from "views/IndexSections/curriculo.js";
 import Habilidades from "views/IndexSections/habilidades.js";
 import Projetos from "views/IndexSections/projetos.js";
-import { Container } from "reactstrap";
 
 export default function Index() {
   React.useEffect(() => {
@@ -21,21 +20,17 @@ export default function Index() {
     };
   }, []);
   return (
-    <Container>
-
+    <>
       <IndexNavbar />
-      <div className="wrapper">
+    
         <PageHeader />
- 
-        <Sobre />
+        <div className="main">
+          <Sobre />
           <Habilidades />
           <Curriculo />
-        
-        <div className="main">
-          
         </div>
         <Footer />
-      </div>
-      </Container>
+   
+    </>
   );
 }
