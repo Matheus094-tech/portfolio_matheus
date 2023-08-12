b0be89156bdb8c7fe11297707c66e72ea3a7e41b
Matheus094-tech <matheusbarbosa094@gmail.com>
2023-08-12 14:16:19 -0300

    ajustados dimensionamento

--- src/views/IndexSections/sobre_mim.js
+++ src/views/IndexSections/sobre_mim.js
@@ -10,27 +10,27 @@ export default function Sobre() {
   return (
     <div className="section" id="sobre-mim">
       <Container>
-        <h2 data-aos="fade-up-left" className="title text-white">
+        <h2 data-aos="fade-up" className="title text-white">
           Sobre mim
         </h2>
-        <p data-aos="fade-up-left" className="text-white">
+        <p data-aos="fade-up" className="text-white">
           Estou na área de TI há 10 anos profissionalmente, especificamente
           desenvolvendo profissionalmente integral estou há 3 anos.
         </p>
         <Row>
           <Col md="12">
             <ul className="list-unstyled mt-2">
-              <li data-aos="fade-up-left" className="py-2">
+              <li data-aos="fade-up" className="py-2">
                 Nascimento:
                 <span class="fw-lighter">25 de Novembro de 1998</span>
               </li>
-              <li data-aos="fade-up-left" className="py-2">
+              <li data-aos="fade-up" className="py-2">
                 Idade: <span class="fw-lighter">24 anos</span>
               </li>
-              <li data-aos="fade-up-left" className="py-2">
+              <li data-aos="fade-up" className="py-2">
                 Estado: <span class="fw-lighter">São Paulo</span>
               </li>
-              <li data-aos="fade-up-left" className="py-2">
+              <li data-aos="fade-up" className="py-2">
                 Certificação mais recente:
                 <a
                   href="https://www.credly.com/badges/91e81db6-d3ff-4bcd-a931-9eacbfb6874e"
@@ -39,7 +39,7 @@ export default function Sobre() {
                   AWS Cloud Practitioner
                 </a>
               </li>
-              <li data-aos="fade-up-left" className="py-2">
+              <li data-aos="fade-up" className="py-2">
                 Trabalho:
                 <a
                   href={newLocal}
@@ -48,7 +48,7 @@ export default function Sobre() {
                   bRHain
                 </a>
               </li>
-              <li data-aos="fade-up-left" className="py-2">
+              <li data-aos="fade-up" className="py-2">
                 Experiência: <span class="fw-lighter"> +2 anos</span>
               </li>
             </ul>
